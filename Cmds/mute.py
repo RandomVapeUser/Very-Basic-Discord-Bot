255488095613abc29e1dcf52cdbceab9e734a299
RandomVapeUser <121972940+RandomVapeUser@users.noreply.github.com>
2024-05-07 19:47:28 +0100

    mute update

--- Cmds/mute.py
+++ Cmds/mute.py
@@ -1,5 +1,5 @@
 from discord.ext import commands
-from datetime import timedelta, datetime
+from datetime import datetime
 import time
 import discord
 
@@ -15,28 +15,39 @@ async def perms(ctx):
 async def mute(ctx, member: discord.Member, time):
     if await perms(ctx) == True:
         return
-    try:
-        separated_list = list(time)
-        int_time = int(separated_list[0])
-        total = {"s": 1, "m": 60, "h": 3600, "d": 86400}
-        total_time = int_time * total[separated_list[1]]
-        
-        if member is None:
-            await ctx.send("The correct usage for this command is:\n**||?mute [USER] [TIME]||**")
+    if member is None:
+            embed = discord.Embed()
+            embed.add_field(name="Mute command",value="`?mute {member} {duration}`")
             return
+    
+    try:
+        time_str = time.lower()
+        if time_str[-1] not in ('s', 'm', 'h', 'd'):
+            await ctx.send("Time not found use s,m,h and d.")
+            return
+        
+        duration_unit = time_str[-1]
+        duration_value = int(time_str[:-1])
 
+        total_seconds = {
+            's': duration_value,
+            'm': duration_value * 60,
+            'h': duration_value * 3600,
+            'd': duration_value * 86400
+        }.get(duration_unit)
+        with open("time.txt","w+") as f:
+            f.write(f"{total_seconds}")
+            f.close()
         seconds = 0
         minutes = 0
         hours = 0
         days = 0
-
-        if separated_list[1] in total:
-            total_seconds = int_time * total[separated_list[1]]
-            duration = timedelta(seconds=total_seconds)
-            await member.timeout(duration)
-            await ctx.send(f"Muted {member} for {duration}")
-        else:
-            await ctx.send("You can only use s,m,h and d are you dumb?")
+        
+        with open("time.txt","r") as f:
+            for i in f:
+                await member.timeout(i)
+                await ctx.send("Timeouted!")
+        await ctx.send(f"Muted {member} for {total_seconds}")
     except Exception as i:
         await ctx.send(f"{i}")
 
